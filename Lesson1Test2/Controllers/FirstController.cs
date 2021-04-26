ebbc0f7b76a7fe554c9ed1fdc047120254241b99
Istomin.I.A <Istomin.I.A@Z4-ISTOMINIA>
2021-04-26 16:00:04 +0300

    Change methods return in FerstController class

--- Lesson1Test2/Controllers/FirstController.cs
+++ Lesson1Test2/Controllers/FirstController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 
 namespace Lesson1Test2.Controllers
@@ -8,40 +7,28 @@ namespace Lesson1Test2.Controllers
     [Route("api/crud")]
     public class FirstController : ControllerBase
     {
-        private List<DataHolder> _holder;
-        
-
-        public FirstController(ValuesHolder holder)
-        {
-            this._holder = ValuesHolder.Values;
-        }
-
         [HttpPost("create")]
         public IActionResult Create([FromQuery] string input)
         {
-            var _holder = ValuesHolder.AddDataHolder(input);
-            return Ok(_holder?.ToString());
+            return Ok(ValuesHolder.AddDataHolder(input).ToString());
         }
 
         [HttpGet("read")]
         public IActionResult Read()
         {
-            return Ok(_holder?.ToString());
+            return Ok(ValuesHolder.Values.ToString());
         }
 
         [HttpPut("update")]
         public IActionResult Update([FromQuery] string dateToUpdate, [FromQuery] string newValue)
         {
-            var _holder = ValuesHolder.UpdateForecast(dateToUpdate, newValue);
-
-            return Ok(_holder?.ToString());
+            return Ok(ValuesHolder.UpdateForecast(dateToUpdate, newValue).ToString());
         }
 
         [HttpDelete("delete")]
         public IActionResult Delete([FromQuery] string stringsToDelete)
         {
-            var _holder = ValuesHolder.DeleteForecast(stringsToDelete);
-            return Ok(_holder?.ToString());
+            return Ok(ValuesHolder.DeleteForecast(stringsToDelete).ToString());
         }
 
         [HttpGet]
--- Lesson1Test2/DataHolder.cs
+++ Lesson1Test2/DataHolder.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace Lesson1Test2
 {
     public class DataHolder
