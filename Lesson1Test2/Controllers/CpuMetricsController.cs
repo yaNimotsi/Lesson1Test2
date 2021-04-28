a992ac2503df13a8f3d64452a401ca0533bbfc85
Istomin.I.A <Istomin.I.A@Z4-ISTOMINIA>
2021-04-28 13:38:57 +0300

    Update CpuMetrics and AgentControllers

--- Lesson1Test2/Controllers/CpuMetricsController.cs
+++ Lesson1Test2/Controllers/CpuMetricsController.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MetricManager.Controllers
 {
@@ -18,26 +14,13 @@ namespace MetricManager.Controllers
         {
             return Ok();
         }
-
-        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
-        public IActionResult GetMetricsByPercentileFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime,
-            [FromRoute] TimeSpan toTime)
-        {
-            return Ok();
-        }
-
+        
         [HttpGet("claster/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAllClaster([FromRoute] TimeSpan fromTime,
             [FromRoute] TimeSpan toTime)
         {
             return Ok();
         }
-
-        [HttpGet("claster/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
-        public IActionResult GetMetricsByPercentileFromAllCluster([FromRoute] TimeSpan fromTime,
-            [FromRoute] TimeSpan toTime, [FromRoute] Percentile percentile)
-        {
-            return Ok();
-        }
+        
     }
 }
