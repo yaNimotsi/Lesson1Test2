a32a49556ec0bb4c79e5b0237957afabc612d62c
Иван Истомин <nimotsiky@gmail.com>
2021-05-05 10:13:02 +0300

    rename folder Lesson1Test2 to MetricsManager

---
+++ Резерв на востановление/Lesson1Test2/Controllers/DotNetMetricsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/dotNet")]
+    [ApiController]
+    public class DotNetMetricsController : ControllerBase
+    {
+        [HttpGet("errors-count/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetErrorsCount([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+    }
+}
