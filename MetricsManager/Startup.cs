d9030eb615d9acdd493aa8c2cd9ea19b8d55eedb
Istomin.I.A <Istomin.I.A@Z4-ISTOMINIA>
2021-05-27 14:25:52 +0300

    add jobs to startUp class

--- MetricsManager/Startup.cs
+++ MetricsManager/Startup.cs
@@ -4,12 +4,19 @@ using FluentMigrator.Runner;
 using MetricsManager.Client;
 using MetricsManager.DAL.ConnectionString;
 using MetricsManager.DAL.Interfaces;
+using MetricsManager.DAL.Jobs;
+using MetricsManager.DAL.Jobs.HostedService;
+using MetricsManager.DAL.Jobs.Jobs;
+using MetricsManager.DAL.Jobs.Schedule;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Polly;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
 
 namespace MetricsManager
 {
@@ -46,6 +53,35 @@ namespace MetricsManager
             services.AddHttpClient<IMetricAgentClient, MetricsAgentClient>()
                 .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(1000)));
 
+            services.AddHostedService<QuartzHostedService>();
+
+            services.AddSingleton<IJobFactory, SingletonJobFactory>();
+            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+
+            services.AddSingleton<CpuMetricJob>();
+            services.AddSingleton(new JobSchedule(
+                typeof(CpuMetricJob),
+                "0/5 * * * * ?"));
+            
+            services.AddSingleton<DotNetMetricJob>();
+            services.AddSingleton(new JobSchedule(
+                typeof(CpuMetricJob),
+                "0/5 * * * * ?"));
+            
+            services.AddSingleton<HddMetricJob>();
+            services.AddSingleton(new JobSchedule(
+                typeof(CpuMetricJob),
+                "0/5 * * * * ?"));
+            
+            services.AddSingleton<NetworkMetricJob>();
+            services.AddSingleton(new JobSchedule(
+                typeof(CpuMetricJob),
+                "0/5 * * * * ?"));
+            
+            services.AddSingleton<RamMetricJob>();
+            services.AddSingleton(new JobSchedule(
+                typeof(CpuMetricJob),
+                "0/5 * * * * ?"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
